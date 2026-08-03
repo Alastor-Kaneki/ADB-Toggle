c86abb0ebdfa41677a99f13359eceb8945d5e7a7
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-02 23:56:02 -0400

    Use palette-driven system bar colors

--- app/src/main/java/com/alastorkaneki/adbtoggle/MainActivity.java
+++ app/src/main/java/com/alastorkaneki/adbtoggle/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
@@ -26,8 +25,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setStatusBarColor(Color.BLACK);
-        getWindow().setNavigationBarColor(Color.BLACK);
         setContentView(R.layout.activity_main);
 
         permissionStatus = findViewById(R.id.permission_status);
